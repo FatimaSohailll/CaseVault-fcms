c4602627dbff0936d9ba822951e9cec57836bcd5
Hiyam <hiyam.rehan44@gmail.com>
2025-11-23 12:45:09 +0500

    screens fix

--- src/main/java/com/fcms/app/SceneManager.java
+++ src/main/java/com/fcms/app/SceneManager.java
@@ -28,10 +28,19 @@ public class SceneManager {
                 return;
             }
 
+            // Set content
             contentArea.getChildren().setAll(content);
+
+            // IMPORTANT: Force loaded screen to fill entire area
+            AnchorPane.setTopAnchor(content, 0.0);
+            AnchorPane.setBottomAnchor(content, 0.0);
+            AnchorPane.setLeftAnchor(content, 0.0);
+            AnchorPane.setRightAnchor(content, 0.0);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+
 }
